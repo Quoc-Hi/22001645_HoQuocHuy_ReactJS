2b67140d46e5b20640b729e229989190ba7f8496
H82M19\Student <Student>
2026-01-28 10:34:31 +0700

    Update buoi1 bai2 add button

--- Tuan2/Bai1/src/App.jsx
+++ Tuan2/Bai1/src/App.jsx
@@ -1,15 +1,27 @@
+import "./App.css";
 import ProductCard from "./components/ProductCard/ProductCard";
+import Button from "./components/Button/Button";
 
 function App() {
   return (
     <div
       style={{
+        padding: "20px",
         display: "flex",
-        justifyContent: "center",
-        marginTop: "50px",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "20px",
       }}
     >
+      {/* Product Card */}
       <ProductCard />
+
+      {/* Buttons */}
+      <div style={{ display: "flex", gap: "10px" }}>
+        <Button type="primary">Primary</Button>
+        <Button type="danger">Delete</Button>
+        <Button type="success">Save</Button>
+      </div>
     </div>
   );
 }
