2de64ef122b8f26195a77c77e5a03769d4396d8d
H82M19\Student <Student>
2026-03-11 10:53:45 +0700

    Bài 2 – Loading & Error State

--- Tuan4/Bai1/src/App.jsx
+++ Tuan4/Bai1/src/App.jsx
@@ -4,18 +4,41 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  useEffect(()=>{
-    const fetchUsers= async ()=>{
-      const response = await fetch("https://jsonplaceholder.typicode.com/users")
-      const data = await response.json()
-      setUsers(data)
-    }
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        setLoading(true);
+
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+
+        if (!response.ok) {
+          throw new Error("API Error");
+        }
+        const data = await response.json();
+        setUsers(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
 
     fetchUsers();
-  
-  },[])
+  },[]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
 
   return (
     <div>
